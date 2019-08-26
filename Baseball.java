25c5836cefb8d9015f537cb07003b0ee9aaeea2c
Nick Rentschler <rentschler.nickolas@ic.edu>
2019-08-26 15:47:03 -0500

    Changes to code

--- Baseball.java
+++ Baseball.java
@@ -14,8 +14,8 @@ class Baseball extends JFrame implements ActionListener {
     private static final int BUTTON_WIDTH   = 100;
     private static final int BUTTON_HEIGHT  = 30;
     private JButton clearButton, computeButton;
-    private JLabel singles, doubles, triples, homeRuns, timesAtBat,  slug, playerName;
-    private JTextField firstInput, secondInput, thirdInput, fourthInput, fifthInput, slugPer, playerInput;
+    private JLabel singles, doubles, triples, homeRuns, timesAtBat,  slug;
+    private JTextField singlesInput, doublesInput, triplesInput, homeRunInput, atBatInput, slugPer;
     
     //Initializes the frame 
     public static void main (String [] args) {
@@ -40,7 +40,6 @@ class Baseball extends JFrame implements ActionListener {
         top.add(new JLabel("Compute slugging percentage"));
         add(top, BorderLayout.NORTH);
         
-        playerName = new JLabel("Enter name of player:        ");
         singles = new JLabel("Number of singles:                ");
         doubles = new JLabel("Number of doubles:                ");
         triples = new JLabel("Number of triples:                   ");
@@ -48,32 +47,32 @@ class Baseball extends JFrame implements ActionListener {
         timesAtBat = new JLabel("Number of times at bat:          ");
         slug = new JLabel("The slugging percentage is:");
         
-       
-        firstInput = new JTextField();
-        firstInput.setColumns(8);
-        secondInput = new JTextField();
-        secondInput.setColumns(8);
-        thirdInput = new JTextField();
-        thirdInput.setColumns(8);
-        fourthInput = new JTextField();
-        fourthInput.setColumns(8);
-        fifthInput = new JTextField();
-        fifthInput.setColumns(8);
+      
+        singlesInput = new JTextField();
+        singlesInput.setColumns(8);
+        doublesInput = new JTextField();
+        doublesInput.setColumns(8);
+        triplesInput = new JTextField();
+        triplesInput.setColumns(8);
+        homeRunInput = new JTextField();
+        homeRunInput.setColumns(8);
+        atBatInput = new JTextField();
+        atBatInput.setColumns(8);
         slugPer = new JTextField();
         slugPer.setColumns(8);
         slugPer.setEditable(false);
      
         JPanel panel = new JPanel();
         panel.add(singles);
-        panel.add(firstInput);
+        panel.add(singlesInput);
         panel.add(doubles);
-        panel.add(secondInput);
+        panel.add(doublesInput);
         panel.add(triples);
-        panel.add(thirdInput);
+        panel.add(triplesInput);
         panel.add(homeRuns);
-        panel.add(fourthInput);
+        panel.add(homeRunInput);
         panel.add(timesAtBat);
-        panel.add(fifthInput);
+        panel.add(atBatInput);
         panel.add(slug);
         panel.add(slugPer);
         add(panel, BorderLayout.CENTER);
@@ -89,28 +88,28 @@ class Baseball extends JFrame implements ActionListener {
     //Action method for the button that is pressed.
     public void actionPerformed(ActionEvent event) {
       if(event.getSource() == computeButton) {
-          int firstBase = Integer.parseInt(firstInput.getText());
-          int secondBase = Integer.parseInt(secondInput.getText());
-          int thirdBase = Integer.parseInt(thirdInput.getText());
-          int  fourthBase = Integer.parseInt(fourthInput.getText());
+          int firstBase = Integer.parseInt(singlesInput.getText());
+          int secondBase = Integer.parseInt(doublesInput.getText());
+          int thirdBase = Integer.parseInt(triplesInput.getText());
+          int  fourthBase = Integer.parseInt(homeRunInput.getText());
           
           secondBase = secondBase * 2;
           thirdBase = thirdBase * 3;
           fourthBase = fourthBase * 4;
           
           int sum = (firstBase + secondBase + thirdBase + fourthBase);
-          int slugg = Integer.parseInt(fifthInput.getText());
+          int slugg = Integer.parseInt(atBatInput.getText());
           double total = (double)sum / (double)slugg;
           
           slugPer.setText(String.format("%.3f", total));
       }
         
       if(event.getSource() == clearButton) {
-          firstInput.setText(" ");
-          secondInput.setText(" ");
-          thirdInput.setText(" ");
-          fourthInput.setText(" ");
-          fifthInput.setText(" ");
+          singlesInput.setText(" ");
+          doublesInput.setText(" ");
+          triplesInput.setText(" ");
+          homeRunInput.setText(" ");
+          atBatInput.setText(" ");
           slugPer.setText(" ");
        }
     }
